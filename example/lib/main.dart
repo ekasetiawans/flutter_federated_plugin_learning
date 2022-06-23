06bf71daf6098bd78b1df72dfdfdfae72245b2ba
Eka Setiawan Saputra <mr.poetra22@gmail.com>
2022-06-23 13:23:29 +0700

    fix: endorsed

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:my_package/my_package.dart';
+import 'package:my_package_flutter/my_package_flutter.dart';
 
 void main() {
   runApp(const MyApp());
---
+++ my_package_flutter/lib/my_package_flutter.dart
@@ -0,0 +1,3 @@
+library my_package_flutter;
+
+export 'package:my_package/my_package.dart';
